6ecb6c69a2171f1c824c77805eab44f725b8788b
shorouq <shrooqabdullahsaad@gmail.com>
2019-03-20 13:34:00 +0200

    modify the console log of runing server

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const port = app.get('port');
 app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+  console.log(`server running on Localhost:${port}`);
 });
